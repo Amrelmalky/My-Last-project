2ec002c481a24e44a68373d3254b0f7ed3543bea
amrelmalky <a.elmalky87@gmail.com>
2023-05-14 22:14:19 +0300

    lesson 6

---
+++ my-app/src/Components/Footer.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+        <footer>
+        Designed & developed by Courses4Arab.com
+        <span>&#129505;  Mizo & Domy </span>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
+
+
